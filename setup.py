b1d7361305dd62ac2eff6ac50724a52908e76c9e
cassiasamp <cassiasampaios@gmail.com>
2022-05-27 18:41:07 -0300

    adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='end to end ml pipeline with mlops tools',
+    author='cassiasamp',
+    license='MIT',
+)
